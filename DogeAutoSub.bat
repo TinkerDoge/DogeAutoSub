3ca9fdaa2f075a399b1ac8ddd4665ffe603fe352
Shibe De Doge <95340998+TinkerDoge@users.noreply.github.com>
2025-01-01 00:05:17 +0700

    Update .gitignore and DogeAutoSub.bat for improved environment setup

--- DogeAutoSub.bat
+++ DogeAutoSub.bat
@@ -2,45 +2,31 @@
 
 set "SCRIPT_DIR=%~dp0"
 set "VENV_DIR=%SCRIPT_DIR%venv"
-set "PYTHON_DIR=%VENV_DIR%\Scripts"
-set "PYTHON=C:\python3.9.7\python.exe"
+set "PYTHON=D:\Software\python310\python.exe"
 set "REQUIREMENTS=%SCRIPT_DIR%requirements.txt"
 set "WHISPER_DIR=%VENV_DIR%\Lib\site-packages\whisper"
-set "CACHE_DIR=%VENV_DIR%\Lib\site-packages\whisper\.cache\whisper"
-set "MODELS_DIR=%SCRIPT_DIR%\modules\models"
 
 if not exist "%VENV_DIR%" (
     echo Creating virtual environment...
     "%PYTHON%" -m venv "%VENV_DIR%"
     call "%VENV_DIR%\Scripts\activate"
     echo Installing/updating requirements...
-    "%PYTHON_DIR%\python.exe" -m pip install --upgrade pip
-    "%PYTHON_DIR%\pip.exe" install --upgrade -r "%REQUIREMENTS%"
+    python -m pip install --upgrade pip
+    python -m pip install --upgrade -r "%REQUIREMENTS%"
     echo Copying custom audio.py and transcribe.py to whisper package...
     copy "%SCRIPT_DIR%modules\audio.py" "%WHISPER_DIR%\audio.py" /Y
     copy "%SCRIPT_DIR%modules\transcribe.py" "%WHISPER_DIR%\transcribe.py" /Y
-    echo Checking and copying pre-downloaded models to cache directory...
-    if not exist "%CACHE_DIR%" mkdir "%CACHE_DIR%"
-    if exist "%MODELS_DIR%" (
-        for %%f in ("%MODELS_DIR%\*") do (
-            if not exist "%CACHE_DIR%\%%~nxf" (
-                echo Copying %%f to cache directory...
-                copy "%%f" "%CACHE_DIR%\" 
-            )
-        )
-    ) else (
-        echo Models folder not found, skipping model copy...
-    )
+
 ) else (
     call "%VENV_DIR%\Scripts\activate"
     echo Updating requirements...
     echo Checking for existing PySide2 installation...
-    "%PYTHON_DIR%\pip.exe" show PySide2 >nul 2>&1
+    python -m pip show PySide2 >nul 2>&1
     if %errorlevel% equ 0 (
         echo Uninstalling existing PySide2...
-        "%PYTHON_DIR%\pip.exe" uninstall -y PySide2
+        python -m pip uninstall -y PySide2
     )
-    "%PYTHON_DIR%\pip.exe" install --upgrade -r "%REQUIREMENTS%"
+    python -m pip install --upgrade -r "%REQUIREMENTS%"
     echo Copying custom audio.py and transcribe.py to whisper package...
     copy "%SCRIPT_DIR%modules\audio.py" "%WHISPER_DIR%\audio.py" /Y
     copy "%SCRIPT_DIR%modules\transcribe.py" "%WHISPER_DIR%\transcribe.py" /Y
@@ -72,7 +58,7 @@ echo        :YYYYYY$$$$$$$$$$$$$$$$$$YYYYYYYiiiiYYYYYYi'
 echo ;       
 
 echo Running AutoUI.py...
-"%PYTHON_DIR%\python.exe" "%SCRIPT_DIR%\AutoUI.py" %*
+python "%SCRIPT_DIR%\AutoUI.py" %*
 if %errorlevel% equ 1 (
   echo The AutoUI.py window was closed unexpectedly.
 ) else (
